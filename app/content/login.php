73b0a6245d780ce7d54d6df22811fb5e72b24b25
lokmen-g-dev <55637661+lokmen-g-dev@users.noreply.github.com>
2024-07-24 17:12:25 +0100

    Update login.php

--- app/content/login.php
+++ app/content/login.php
@@ -1,24 +1,31 @@
 <div class="row">
     <div class="col-lg-12">
         <?php
-        if(isset($_POST['email']) && isset($_POST['password'])) {
+        session_start();
+        
+        if (isset($_POST['email']) && isset($_POST['password'])) {
             try {
+                // Sanitize user inputs
+                $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+                $password = $_POST['password'];
+
                 // login
-                if($result = $model->userSignIn($_POST['email'], $_POST['password'], $config['system']['hashing_algorithm'])) {
-                    if(false !== $result && 0 < count($result)) {
+                if ($result = $model->userSignIn($email, $password, $config['system']['hashing_algorithm'])) {
+                    if (false !== $result && count($result) > 0) {
                         // delete session data
                         session_unset();
 
                         // save user to session
                         $_SESSION['user_id'] = $result[0]['id'];
+
+                        // Redirect to logged in page
+                        header('Location: ?page=loggedin');
+                        exit;
+                    } else {
+                        echo '<div class="alert alert-danger">Wrong E-Mail-Address or Password</div>';
                     }
-                    redirect('?page=loggedin');
                 }
-                else {
-                    echo '<div class="alert alert-danger">Wrong E-Mail-Address or Password</div>';
-                }
-            }
-            catch (Exception $ex) {
+            } catch (Exception $ex) {
                 error(500, 'Exception during login', $ex);
             }
         }
