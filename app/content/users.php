a2ca9718182434e2b94cf7cdfe8f6cacaf731077
lokmen-g-dev <55637661+lokmen-g-dev@users.noreply.github.com>
2024-07-24 17:13:05 +0100

    Update users.php

--- app/content/users.php
+++ app/content/users.php
@@ -1,6 +1,8 @@
 <?php
+session_start();
+
 // make sure only registered users can enter this site
-if(!isset($_SESSION['user_id']) || 1 != $_SESSION['user']['is_admin']) {
+if (!isset($_SESSION['user_id']) || 1 != $_SESSION['user']['is_admin']) {
     echo getForbiddenMessage('This Site is for Administrators only.');
     return;
 }
@@ -10,22 +12,21 @@ $deleted = null;
 // delete user
 try {
     if (isset($_GET['remove'])) {
-        if ($_GET['remove'] !== $_SESSION['user_id']) {
-            $deleted = $model->removeUser($_GET['remove']);
+        $removeUserId = intval($_GET['remove']);
+        if ($removeUserId !== $_SESSION['user_id']) {
+            $deleted = $model->removeUser($removeUserId);
         } else {
             $deleted = false;
         }
     }
-}
-catch(Exception $ex) {
+} catch (Exception $ex) {
     error(500, 'Could not delete given User from Database', $ex);
 }
 
 // get all users
 try {
     $result = $model->getAllUsers();
-}
-catch(Exception $ex) {
+} catch (Exception $ex) {
     error(500, 'Could not query Users from Database', $ex);
 }
 ?>
@@ -35,15 +36,14 @@ catch(Exception $ex) {
         <h1>Administration</h1>
 
         <?php
-        if($deleted === true) {
+        if ($deleted === true) {
             echo '<div class="alert alert-success">The user has been deleted.</div>';
-        }
-        elseif($deleted === false) {
+        } elseif ($deleted === false) {
             echo '<div class="alert alert-danger">The user could not be deleted.</div>';
         }
         ?>
 
-        <?php if(false !== $result && 0 < count($result)) { ?>
+        <?php if (false !== $result && 0 < count($result)) { ?>
             <table class="table">
                 <thead>
                 <tr>
@@ -55,21 +55,33 @@ catch(Exception $ex) {
                     <th>Actions</th>
                 </tr>
                 </thead>
+                <tbody>
                 <?php foreach ($result as $user) { ?>
                     <tr>
-                        <td><?php echo $user['id']; ?></td>
-                        <td><a href="/?page=profile&id=<?php echo $user['id']; ?>"><?php echo $user['username']; ?></a></td>
-                        <td><?php echo $user['email']; ?></td>
-                        <td><?php echo $user['country']; ?></td>
+                        <td><?php echo htmlspecialchars($user['id']); ?></td>
+                        <td><a href="/?page=profile&id=<?php echo htmlspecialchars($user['id']); ?>"><?php echo htmlspecialchars($user['username']); ?></a></td>
+                        <td><?php echo htmlspecialchars($user['email']); ?></td>
+                        <td><?php echo htmlspecialchars($user['country']); ?></td>
                         <td><?php echo (1 == $user['is_admin'] ? icon('ok') : ''); ?></td>
                         <td>
-                            <a href="?page=editprofile&id=<?php echo $user['id']; ?>" class="btn btn-default" title="Edit User"><?php echo icon('edit'); ?> Edit</a>
-                            <a href="?page=users&remove=<?php echo $user['id']; ?>" class="btn btn-default remove-user" title="Remove User"><?php echo icon('remove'); ?> Remove</a>
+                            <a href="?page=editprofile&id=<?php echo htmlspecialchars($user['id']); ?>" class="btn btn-default" title="Edit User"><?php echo icon('edit'); ?> Edit</a>
+                            <a href="?page=users&remove=<?php echo htmlspecialchars($user['id']); ?>" class="btn btn-default remove-user" title="Remove User" onclick="return confirm('Are you sure you want to remove this user?');"><?php echo icon('remove'); ?> Remove</a>
                         </td>
                     </tr>
                 <?php } ?>
+                </tbody>
             </table>
         <?php } ?>
 
     </div>
 </div>
+
+<script>
+    document.querySelectorAll('.remove-user').forEach(function(element) {
+        element.addEventListener('click', function(event) {
+            if (!confirm('Are you sure you want to remove this user?')) {
+                event.preventDefault();
+            }
+        });
+    });
+</script>
